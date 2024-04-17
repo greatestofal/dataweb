d143468b5c4162b54ed87207f57e147c4d796f2d
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-17 10:18:57 +0100

    Added few components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Analytics from "./component/Analytics";
 import Hero from "./component/Hero";
 import Navbar from "./component/Navbar";
+import Newsletter from "./component/Newsletter";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </>
   );
 }
---
+++ src/component/Cards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-3"> </div>
+    </div>
+  );
+};
+
+export default Cards;
